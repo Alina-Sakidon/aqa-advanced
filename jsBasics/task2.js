f6b7baedf927effd8cb86b5b412f84afc05faac5
asakidon <alinasakidon@gmail.com>
2025-03-03 19:54:31 +0200

    Finished home work jsBasics

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let namePerson1 = "Anna";
+let namePerson2 = "Alina";
+
+let concatedNamesHello = "Hello, " + namePerson1 + " and " + namePerson2 + " !";
+console.log(concatedNamesHello);
+
+let templateedNamesHello = `Hello, ${namePerson1} and ${namePerson2} !`;
+console.log(templateedNamesHello);
