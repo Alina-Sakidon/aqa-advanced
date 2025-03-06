909689be4f10ac6639609d693c30d69073172b37
asakidon <alinasakidon@gmail.com>
2025-03-06 12:03:05 +0200

    Finished home work with loops and conditions

---
+++ conditionsAndLoops/task1.js
@@ -0,0 +1,18 @@
+const averageGrade = 71;
+
+if (averageGrade < 60) {
+    console.log("Unsatisfactorily");
+}
+else if (averageGrade >= 60 && averageGrade <= 70) {
+    console.log("Satisfactorily");
+} else if (averageGrade >= 71 && averageGrade <= 80) {
+    console.log("Good");
+} else if (averageGrade >= 81 && averageGrade <= 90) {
+    console.log("Very good");
+}
+else if (averageGrade >= 91 && averageGrade <= 100) {
+    console.log("Excellent");
+}
+else {
+    console.log("For an objective result, the avarage grade should be rounded to the nearest whole number between 0 and 100.");
+}
