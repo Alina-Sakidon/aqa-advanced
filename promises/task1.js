a67e140902766825c403dd6a945a06be642d8f15
asakidon <alinasakidon@gmail.com>
2025-04-01 20:28:03 +0300

    Reformat code with prettier

--- promises/task1.js
+++ promises/task1.js
@@ -1,9 +1,9 @@
 function printText(text, miliseconds) {
-  setTimeout(() => {
-    console.log(text);
-  }, miliseconds);
+	setTimeout(() => {
+		console.log(text);
+	}, miliseconds);
 }
 
-printText("Hello", 1000);
-printText("World", 2000);
-printText("!", 3000);
+printText('Hello', 1000);
+printText('World', 2000);
+printText('!', 3000);
