f902dddf7b301ac0ee0a8e96c1c1ed54ead5d254
asakidon <alinasakidon@gmail.com>
2025-04-08 19:14:53 +0300

    Add task1 and task2

---
+++ axiosAndJest/fetchData.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts/wrongUrl');
+        console.log(response.data);
+        return response.data;
+    }
+
+catch (error) {
+        console.error('Error fetching data:', error.message);
+        throw new Error('Request failed with status: ' + (error.response?.status || 'Unknown error'));
+    }
+}
+
+async function fetchDataWithHeadersAndParam(url, headers, params) {
+    try {
+      const response = await axios.get(url, {
+        headers: headers, // Кастомні заголовки
+        params: params,   // Кастомні параметри URL
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error('Request failed: ' + error.message);
+    }
+  }
+
+export { fetchDataWithHeadersAndParam, fetchData };
